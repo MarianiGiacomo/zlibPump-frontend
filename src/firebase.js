e7a9b67cdaa7e9712b6a9237f7005d03e2be8692
Giacomo Mariani <giacomo.mariani@aalto.fi>
2020-12-28 17:16:47 +0200

    testing for env varaibles

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase'
 
 console.log('INPUT_APIKEY', process.env.INPUT_FIREBASE_ZLIBPUMP_APIKEY);
-console.log('INOUT_PROJECTID', process.env.INPUT_PROCESS_ID);
+console.log('INPUT_PROJECTID', process.env.INPUT_PROJECTID);
 console.log('APIKEY', process.env.FIREBASE_ZLIBPUMP_APIKEY);
-console.log('PROJECTID', process.env.FIREBASE_PROJECT_ID);
+console.log('PROJECTID', process.env.PROJECT_ID);
 console.log('channels', process.env.FIREBASE_CLI_PREVIEWS)
 console.log('env variables', process.env);
 
